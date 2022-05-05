998277fc08271449803469bbc89bf0324cb2bda6
emitacek <mitacek2@hotmail.com>
2022-05-05 14:09:32 -0500

    Procedurally spread inputs

--- src/App.js
+++ src/App.js
@@ -4,21 +4,37 @@ import React from 'react'
 import { useState } from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import InputField from './InputField/InputField';
+import GenericForm from './GenericForm/GenericForm';
 
 const App = () => {
-  const [elevatedState, setElevatedState] = useState('second');
+  const [fakeStackIndex, setFakeStackIndex] = useState(0);
+  const [elevatedState, setElevatedState] = useState('This is an app level statehook');
+  const trialDataArray = [
+    {
+      name: 'First one',
+      title: 'Type something'
+    },
+    {
+      name: 'secondOne',
+      title: 'This one has a default value',
+      initValue: 'Second'
+    },
+    {
+      name: 'First one',
+      initValue: 'this is testing our default title appending.'
+    }
+  ];
+  const commitToSessionStorage = (commitMe) => {
+    sessionStorage.setItem(fakeStackIndex, JSON.stringify(commitMe));
+    setFakeStackIndex(prev => prev + 1);
+  };
   return (
     <div>
       App
-      <InputField
-        name='First one'
-        title='Just type something'
-        initValue={'Anythings fine'}
-      />
-      <InputField
-        name='second one'
-        title='A second field'
-        initValue={'Will it work? it should'}
+      <GenericForm
+        formSource={trialDataArray}
+        submitText={'Click and Be Judged!'}
+        submitAction={commitToSessionStorage}
       />
       <InputField
         name='a third one'
--- src/GenericForm/GenericForm.js
+++ src/GenericForm/GenericForm.js
@@ -1,9 +1,46 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import InputField from '../InputField/InputField';
 
 const GenericForm = ({ formSource, submitText, submitAction }) => {
+    const formInitState = {};
+    const propertyNameArray = [];
+    formSource.forEach((entity) => {
+        let tentativePropertyName = entity.name;
+        while (tentativePropertyName !== undefined && tentativePropertyName !== null) {
+            if (formInitState[tentativePropertyName] !== undefined) {
+                tentativePropertyName = tentativePropertyName + tentativePropertyName.length;
+            }
+            else {
+                formInitState[tentativePropertyName] = entity.initValue || '';
+                propertyNameArray.push(tentativePropertyName);
+                tentativePropertyName = null; //exits loop;
+            }
+        }
+    });
+    const [formData, setFormData] = useState(formInitState);
+
     return (
-        <div>GenericForm</div>
+        <div>
+            {formSource.map(entity => {
+                if (entity.name !== undefined && entity.name !== null) {
+                    const propertyName = propertyNameArray.shift();
+                    return (<InputField
+                        key={`input-${propertyName}`}
+                        name={propertyName}
+                        title={entity.title || `Enter value for ${propertyName}`}
+                        value={formData[propertyName]}
+                        changeCall={e => setFormData(prev => {
+                            const update = { ...prev };
+                            update[propertyName] = e.target.value;
+                            return update
+                        })}
+                    />)
+                }
+                return (<></>);
+            })}
+            <button onClick={e => submitAction(formData)}>{submitText}</button>
+        </div >
     );
 };
 
@@ -14,8 +51,8 @@ GenericForm.propTypes = {
 };
 
 GenericForm.defaultProps = {
-    submitText:'Submit',
-    submitAction: ()=>{},
+    submitText: 'Submit',
+    submitAction: () => { },
 };
 
 export default GenericForm;
